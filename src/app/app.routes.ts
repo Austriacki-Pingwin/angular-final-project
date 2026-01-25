10e4f931973d9bdae22efce0299937460b4789c8
Nikita <snitkonikita@gmail.com>
2026-01-25 17:22:59 +0100

    feat: add basic router and arhitecture for auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,30 @@
 import type { Routes } from '@angular/router';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { SignInPageComponent } from './components/auth-page/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './components/auth-page/sign-up-page/sign-up-page.component';
+import { ForgotPasswordPageComponent } from './components/auth-page/forgot-password-page/forgot-password-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: WelcomePageComponent,
+  },
+  {
+    path: 'auth',
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
+      {
+        path: 'sign-in',
+        component: SignInPageComponent,
+      },
+      {
+        path: 'sign-up',
+        component: SignUpPageComponent,
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPasswordPageComponent,
+      },
+    ],
   },
 ];
--- src/app/components/welcome-page/welcome-page.component.ts
+++ src/app/components/welcome-page/welcome-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-page',
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss',
 })
