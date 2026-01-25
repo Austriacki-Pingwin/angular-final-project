77ae62c7217374bf5d89447728494acae95b1e05
Nikita <snitkonikita@gmail.com>
2026-01-25 16:04:37 +0100

    feat: set up bootstrap

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss',
 })
-export class WelcomePageComponent {
-
-}
+export class WelcomePageComponent {}
