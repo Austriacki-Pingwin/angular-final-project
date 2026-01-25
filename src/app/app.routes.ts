70259dab47e054d0d82ddd17707639e4fdab67fe
Nikita <snitkonikita@gmail.com>
2026-01-25 15:56:32 +0100

    feat: add project, basic setting, material angular, basic template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import type { Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomePageComponent,
+  },
+];
