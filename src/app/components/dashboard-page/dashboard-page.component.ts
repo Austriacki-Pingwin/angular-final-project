ee974574bbf46a87cd1dfdfbfcec9238acd5e194
Nikita <snitkonikita@gmail.com>
2026-01-26 17:08:08 +0100

    fix: module matIcon

--- src/app/components/dashboard-page/dashboard-page.component.ts
+++ src/app/components/dashboard-page/dashboard-page.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { type User } from '@angular/fire/auth';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIcon],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss',
 })
