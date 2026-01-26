3328f0f071623d34d91cdcb96445ee0d391ad531
Nikita <snitkonikita@gmail.com>
2026-01-26 12:23:44 +0100

    feat: add basic logic for sign-in with email and password

--- src/app/components/auth-page/sign-in-page/sign-in-page.component.ts
+++ src/app/components/auth-page/sign-in-page/sign-in-page.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sign-in-page',
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, RouterModule],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIcon,
+  ],
   templateUrl: './sign-in-page.component.html',
   styleUrl: './sign-in-page.component.scss',
 })
-export class SignInPageComponent {}
+export class SignInPageComponent {
+  private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
+
+  public errorMessage = this.authService.errorMessage;
+  public isSubmissionInProgress = this.authService.isSubmissionInProgress;
+
+  public form = this.formBuilder.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  public onSubmit(): void {
+    if (this.form.invalid) return;
+    this.authService._isSubmissionInProgress.set(true);
+    const form = this.form.getRawValue();
+    this.authService.signInWithEmailAndPassword(form);
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,54 @@
+import { inject, Injectable, signal } from '@angular/core';
+import {
+  Auth,
+  AuthErrorCodes,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private router = inject(Router);
+  private _errorMessage = signal<string>('');
+  public _isSubmissionInProgress = signal<boolean>(false);
+
+  // * init the google auth provider
+  public googleAuthProvider = new GoogleAuthProvider();
+
+  // * auth instance
+  public auth = inject(Auth);
+
+  public readonly errorMessage = this._errorMessage.asReadonly();
+  public isSubmissionInProgress = this._isSubmissionInProgress.asReadonly();
+
+  public signInWithEmailAndPassword(form: { email: string; password: string }): void {
+    signInWithEmailAndPassword(this.auth, form.email, form.password)
+      .then(() => {
+        this.redirectToDashboard();
+      })
+      .catch((error) => {
+        this._isSubmissionInProgress.set(false);
+        console.error('error: ', error);
+        if (error instanceof Error) {
+          if (error.message.includes(AuthErrorCodes.INVALID_EMAIL)) {
+            this._errorMessage.set('Email is not valid');
+          } else if (error.message.includes(AuthErrorCodes.INVALID_LOGIN_CREDENTIALS)) {
+            this._errorMessage.set('Invalid Email/Password');
+          } else if (error.message.includes(AuthErrorCodes.WEAK_PASSWORD)) {
+            this._errorMessage.set('Please enter a stronger password');
+          } else if (error.message.includes(AuthErrorCodes.EMAIL_EXISTS)) {
+            this._errorMessage.set('The email is already used for another account');
+          } else {
+            this._errorMessage.set('Something went wrong, please try again');
+          }
+        }
+      });
+  }
+
+  public redirectToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+}
