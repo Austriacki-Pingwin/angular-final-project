51740ca55cc7df9068c0e7120e99a544e21d5db9
Anna Vasilevich <dzichonka@gmail.com>
2026-02-09 12:27:46 +0100

    feat: change @Input, @Output to input? output

--- src/app/components/profile-page/profile-form/tabs/tabs.component.ts
+++ src/app/components/profile-page/profile-form/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 
 import { PROFILE_TABS } from './tabs.model';
@@ -12,11 +12,14 @@ import { type ProfileBlockType } from '../../../../models/collections.model';
   styleUrl: './tabs.component.scss',
 })
 export class ProfileTabsComponent {
-  @Input({ required: true })
-  public activeTab!: ProfileBlockType;
+  public activeTab = input.required<ProfileBlockType>();
+  public tabChange = output<ProfileBlockType>();
 
-  @Output()
-  public tabChange = new EventEmitter<ProfileBlockType>();
+  // @Input({ required: true })
+  // public activeTab!: ProfileBlockType;
+
+  // @Output()
+  // public tabChange = new EventEmitter<ProfileBlockType>();
 
   public readonly tabs = PROFILE_TABS;
 
@@ -25,7 +28,7 @@ export class ProfileTabsComponent {
   }
 
   public get selectedIndex(): number {
-    const index = this.tabs.findIndex((t) => t.key === this.activeTab);
+    const index = this.tabs.findIndex((t) => t.key === this.activeTab());
     return index === -1 ? 0 : index;
   }
 }
