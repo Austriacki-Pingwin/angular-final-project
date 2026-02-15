6e231b6686ff9a32e5897f9630e3cbe3a034fe3f
Nikita <snitkonikita@gmail.com>
2026-02-15 23:50:14 +0100

    fix: minor fix

--- src/app/components/preview/item-block/personal-item/personal-item.component.ts
+++ src/app/components/preview/item-block/personal-item/personal-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { type Personal } from '../../../../models/blocks.model';
 import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-personal-item',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './personal-item.component.html',
   styleUrl: './personal-item.component.scss',
 })
