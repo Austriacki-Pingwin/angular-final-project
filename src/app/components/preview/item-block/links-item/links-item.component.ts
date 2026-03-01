7c9f4d36fbc10cefafe03986b0d1b4b5522ef994
Nikita <snitkonikita@gmail.com>
2026-03-01 23:36:11 +0100

    feat: add drag and drop for preview items

--- src/app/components/preview/item-block/links-item/links-item.component.ts
+++ src/app/components/preview/item-block/links-item/links-item.component.ts
@@ -1,14 +1,20 @@
 import { Component, input } from '@angular/core';
 import { type Link } from '../../../../models/blocks.model';
 import { CommonModule } from '@angular/common';
+import { DragDropModule, moveItemInArray, type CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-links-item',
-  imports: [CommonModule],
+  imports: [CommonModule, DragDropModule],
   templateUrl: './links-item.component.html',
   styleUrl: './links-item.component.scss',
   host: { class: 'links' },
 })
 export class LinksItemComponent {
   public data = input.required<Link[]>();
+
+  public linksDrop(event: CdkDragDrop<Link[]>): void {
+    const current = this.data();
+    moveItemInArray(current, event.previousIndex, event.currentIndex);
+  }
 }
