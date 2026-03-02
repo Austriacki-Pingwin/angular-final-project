32deb914c3c39be896780bdf58fbffb715a77ec4
Paul Pavlov <pavlov1space@gmail.com>
2026-03-02 22:04:20 +0100

    "test: add component tests for CvCardComponent and fix service mocks

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,25 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
+
   transform: {
-    '^.+\\.(ts|mjs|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|js|mjs|html)$': 'jest-preset-angular',
   },
+
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+  },
+
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+
   collectCoverage: true,
   coverageDirectory: 'coverage',
 };
